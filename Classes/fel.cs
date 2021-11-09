115bd9008cdcd7d548a9e8a7ad867c64ffab015e
Victoria Hering Röhr <victoriaerohr@gmail.com>
2021-11-09 14:12:20 +0100

    a

---
+++ Classes/fel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversion.Classes
+{
+    public class fel
+    {
+        public static double Fel()
+        {
+            Console.WriteLine("Fel inmatning, tryck på valfri tangent för att göra rätt val.");
+            Console.ReadKey();
+            Lines.Line();
+            NewConvert.ConvertAgain();
+        }
+    }
+}
